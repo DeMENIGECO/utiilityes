3bb71b1a866d61dee906c48a9b3423459d83918f
DeMENIGECO <domenigeco@gmail.com>
2026-02-16 16:08:14 +0100

    Update run32.py

--- utiilityes/run32.py
+++ utiilityes/run32.py
@@ -1,45 +1,19 @@
-from .index import Index as ind
+from .index import Index
 
-def log(message):
+def log(message: str):
     print(message)
 
-def log_inui(message):
-    ui = ind()
-    ui.init.win("Log")\
-      .size(300, 150)
-
-    ui.run_funct()\
-      .text(message)\
-      .render()
-
-def multiple_log(*objects):
-    for obj in objects:
-        print(obj)
+def log_inui(message: str):
+    ui = Index()
+    ui.init.win("Log").size(300, 150)
+    ui.run_funct().text(str(message)).render()
 
 def multiple_log_inui(*objects):
-    ui = ind()
-    ui.init.win("Logs")\
-      .size(300, 200)
-
+    ui = Index()
+    ui.init.win("Logs").size(300, 200)
     f = ui.run_funct()
     for obj in objects:
         f.text(str(obj))
     f.render()
 
-def example():
-    def saluto():
-        print("Hai cliccato SALUTA!")
 
-    def esci():
-        print("Esco...")
-        raise SystemExit
-
-    ui = ind()
-    ui.init.win("Example")\
-      .size(600, 400)
-
-    ui.run_funct()\
-      .text("Benvenuto nella GUI SDL2!")\
-      .button("Saluta", action=saluto)\
-      .button("Esci", action=esci)\
-      .render()
