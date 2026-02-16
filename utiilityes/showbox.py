1c1bbf6f3814eea44db0a41cd1f1db5a50bac22c
DeMENIGECO <domenigeco@gmail.com>
2026-02-16 16:08:41 +0100

    Update showbox.py

--- utiilityes/showbox.py
+++ utiilityes/showbox.py
@@ -1,48 +1,37 @@
-from .index import Index as ind
+from .index import Index
 from .run32 import log
 
-def error(message):
-    ui = ind()
-    ui.init.win("Errore")\
-      .size(300, 150)
+def error(message: str):
+    ui = Index()
+    ui.init.win("Errore").size(300, 150)
+    ui.run_funct().text(str(message)).render()
 
-    ui.run_funct()\
-      .text(message)\
-      .render()
+def info(message: str):
+    ui = Index()
+    ui.init.win("Informazione").size(300, 150)
+    ui.run_funct().text(str(message)).render()
 
-def info(message):
-    ui = ind()
-    ui.init.win("Informazione")\
-      .size(300, 150)
+def warning(message: str):
+    ui = Index()
+    ui.init.win("Attenzione").size(300, 150)
+    ui.run_funct().text(str(message)).render()
 
-    ui.run_funct()\
-      .text(message)\
-      .render()
-
-def warning(message):
-    ui = ind()
-    ui.init.win("Attenzione!")\
-      .size(300, 150)
-
-    ui.run_funct()\
-      .text(message)\
-      .render()
-
-def confirm(message):
-    ui = ind()
-    ui.init.win("Conferma?")\
-      .size(300, 150)
+def confirm(message: str):
+    ui = Index()
+    ui.init.win("Conferma").size(300, 150)
 
     def si():
-        log("Accettato!")
+        log("Confermato")
 
     def no():
         raise SystemExit
 
     ui.run_funct()\
-      .text(message)\
+      .text(str(message))\
       .button("Continua", action=si)\
       .button("Annulla", action=no)\
       .render()
+
+
 
 
