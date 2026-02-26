05198c3ddc26f1cf12e7b1ed0ba307752526ec15
DeMENIGECO <domenigeco@gmail.com>
2026-02-26 16:53:36 +0100

    Update showbox.py

--- utiilityes/showbox.py
+++ utiilityes/showbox.py
@@ -1,23 +1,29 @@
 from .index import Index
 from .run32 import log
+from .run32 import _current_ui  # importiamo il riferimento
+from .run32 import _new_ui      # importiamo la funzione
+
 
 def error(message: str):
-    ui = Index()
+    ui = _new_ui()
     ui.init.win("Errore").size(300, 150)
     ui.run_funct().text(str(message)).render()
 
+
 def info(message: str):
-    ui = Index()
+    ui = _new_ui()
     ui.init.win("Informazione").size(300, 150)
     ui.run_funct().text(str(message)).render()
 
+
 def warning(message: str):
-    ui = Index()
+    ui = _new_ui()
     ui.init.win("Attenzione").size(300, 150)
     ui.run_funct().text(str(message)).render()
 
+
 def confirm(message: str):
-    ui = Index()
+    ui = _new_ui()
     ui.init.win("Conferma").size(300, 150)
 
     def si():
@@ -31,7 +37,6 @@ def confirm(message: str):
       .button("Continua", action=si)\
       .button("Annulla", action=no)\
       .render()
-
 
 
 
